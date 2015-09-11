a122e420b6fec4e0519d91ae5b1665cf31a36f85
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2015-09-10 21:51:24 -0400

    Try using gcc-5

--- script.sh
+++ script.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 case "$CC" in
-    "gcc"   ) SPEC=g++   ;;
-    "clang" ) SPEC=clang ;;
+    "gcc")
+    SPEC=g++
+    export CC=gcc-5
+    export CXX=g++-5
+    ;;
+
+    "clang")
+    SPEC=clang
+    ;;
 esac
 
 qmake ./BALLS/BALLS.pro -r -spec linux-$SPEC
