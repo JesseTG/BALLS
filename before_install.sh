c758b182a80be4fb2b47bc1a0bc01c5741fd3b79
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2015-09-23 15:05:40 -0400

    Clone qt 5.6
    Add configure support for some libraries

--- before_install.sh
+++ before_install.sh
@@ -15,19 +15,26 @@ clone() {
     name=$1
     url=$2
     branch=$3
+    flags=$4
     if [[ ! -e $name ]]
     then
         echo "Cloning the latest version of $name"
         git clone -q $url
+
+        cd $name
         git checkout $branch
 
         if [[ -e $name/CMakeLists.txt ]]
         then
             echo "$url is a CMake project"
-            cd $name
-            cmake -DCMAKE_INSTALL_PREFIX:FILEPATH="~/deps"
-            cd ..
+            cmake -DCMAKE_INSTALL_PREFIX:FILEPATH="~/deps" $flags
+        elif [[ -e $name/configure ]]
+        then
+            echo "$url uses configure"
+            ./configure $flags
         fi
+
+        cd ..
 
     else
         echo "Not cloning $name, it's in the cache"
@@ -37,3 +44,53 @@ clone() {
 cd ~/repos
 clone glm https://github.com/g-truc/glm.git master
 clone gli https://github.com/g-truc/gli.git master
+clone qt5 http://code.qt.io/qt/qt5.git 5.6
+
+cd ~/repos/qt5
+
+if [[ ! -e Makefile ]]
+then
+    ./init-repository --module-subset=qtbase,qt3d,qtimageformats
+    ./configure \
+        -c++std c++11 \
+        -confirm-license \
+        -nomake examples \
+        -nomake tests \
+        -opensource \
+        -optimized-qmake \
+        -pch \
+        -platform linux-g++ \
+        -prefix="~/deps/qt5" \
+        -qt-xcb \
+        -silent \
+        -static \
+        -no-alsa \
+        -no-cups \
+        -no-dbus \
+        -no-gstreamer \
+        -no-largefile \
+        -no-libproxy \
+        -no-nis \
+        -no-openssl \
+        -no-pulseaudio \
+        -no-qml-debug \
+        -no-sql-mysql \
+        -no-sql-psql \
+        -no-sql-sqlite \
+        -no-tslib \
+
+    # The disabled libraries above will be re-enabled if:
+    # cups: We implement printing of source code
+    # dbus: We call on another program and it needs to talk to us
+    # gstreamer: We use any sort of multimedia
+    # nis/openssl/libproxy: We do any sort of networking
+    # pulseaudio/alsa: We use any sort of audio
+    # qml-debug: We use QML
+    # sql: We keep track of data in a structured manner and QSettings won't cut it
+    # tslib: We need touch screen-specific logic
+    # (Can't put these comments next to the things they actually reference. Blame bash.)
+
+    make -s -j
+fi
+
+cd ..
