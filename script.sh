f9384f1233339ed42599ea73d60663d3f98e7638
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2015-10-12 18:43:57 -0400

    alias
    - Of course!  I think

--- script.sh
+++ script.sh
@@ -5,14 +5,19 @@ case "$CC" in
     SPEC=g++
     export CC=gcc-5
     export CXX=g++-5
+    alias gcc=gcc-5
+    alias g++=g++-5
     ;;
 
     "clang")
     SPEC=clang
     export CC=clang-3.8
     export CXX=clang++-3.8
+    alias clang=clang-3.8
+    alias clang++=clang++-3.8
     ;;
 esac
 
+alias -p
 /opt/qt55/bin/qmake ./BALLS/BALLS.pro -spec linux-$SPEC
 make -s
