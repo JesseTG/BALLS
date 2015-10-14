181fef20e173b9e1145ba08a1681c34f468fa122
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2015-10-13 21:57:23 -0400

    Let's try a new thing with YAML
    - Maybe matrices will suit me better

--- before_install.sh
+++ before_install.sh
@@ -34,34 +34,20 @@ clone() {
     popd
 }
 
-export COMMON_PACKAGES="qt553d qt55base libboost-dev libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libqt5scintilla2-dev libqt5scintilla2-l10n libqt5scintilla2-11"
-case "$CC" in
-    "gcc")
-    export PLATFORM_PACKAGES="gcc-5 g++-5 cpp-5"
-    ;;
-
-    "clang")
-    export PLATFORM_PACKAGES="clang-3.7 llvm-3.7 llvm-3.7-dev libclang-3.7-dev libc++-dev libc++abi-dev libasan2"
-    sudo add-apt-repository -y 'deb http://llvm.org/apt/jessie/ llvm-toolchain-jessie-3.7 main'
-    ;;
-esac
-
+sudo add-apt-repository -y 'deb http://llvm.org/apt/jessie/ llvm-toolchain-jessie-3.7 main'
 sudo add-apt-repository -y 'deb http://ppa.launchpad.net/beineri/opt-qt55-trusty/ubuntu trusty main'
 sudo apt-get -qqy update
 sudo apt-get install --no-install-recommends --force-yes --no-upgrade -qqy $COMMON_PACKAGES $PLATFORM_PACKAGES
 sudo chmod -f +x /opt/qt55/bin/qt55-env.sh
 /opt/qt55/bin/qt55-env.sh
 
-case "$CC" in
-    "gcc")
+if [[ $CC == *"gcc"* ]]; then
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 100
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 100
 
     sudo update-alternatives --auto g++
     sudo update-alternatives --auto gcc
-    ;;
-
-    "clang")
+elif [[ $CC == *"clang"* ]]; then
     sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.7 100
     sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.7 100
     sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 1000
@@ -69,8 +55,7 @@ case "$CC" in
     sudo update-alternatives --auto clang++
     sudo update-alternatives --auto clang
     sudo update-alternatives --auto c++
-    ;;
-esac
+fi
 
 clone glm https://github.com/g-truc/glm.git master
 clone gli https://github.com/g-truc/gli.git master
--- script.sh
+++ script.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-case "$CC" in
-    "gcc")
-    SPEC=g++
-    ;;
-
-    "clang")
-    SPEC=clang
-    ;;
-esac
-
-
 $CC --version
 $CXX --version
 /opt/qt55/bin/qmake ./BALLS/BALLS.pro -spec linux-$SPEC
