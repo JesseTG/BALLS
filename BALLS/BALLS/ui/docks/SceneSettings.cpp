7e3906308ca1444eecfbbf6f23fd7734b769fae3
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-02-22 15:13:24 -0500

    Split a few docks off to separate widgets
    - Can't edit BallsWindow.ui if it's too slow to do anything

--- BALLS/BALLS/ui/docks/SceneSettings.cpp
+++ BALLS/BALLS/ui/docks/SceneSettings.cpp
@@ -1,11 +1,23 @@
 #include "precompiled.hpp"
-#include "ui/docks/SceneSettings.hpp"
+
+#include "SceneSettings.hpp"
+
+#include <QOpenGLContext>
+#include "ui/BallsCanvas.hpp"
 
 namespace balls {
 
-SceneSettings::SceneSettings(QWidget* parent) :
-  QDockWidget(parent)
-{
+SceneSettings::SceneSettings(QWidget *parent) : QWidget(parent) {
   ui.setupUi(this);
 }
+
+void SceneSettings::initCanvas(BallsCanvas *canvas) {
+  connect(ui.meshComboBox, SIGNAL(currentIndexChanged(int)), canvas->window(), SLOT(setMesh(const int)));
+  connect(ui.wireframeCheck, SIGNAL(toggled(bool)), canvas, SLOT(setOption(bool)));
+  connect(ui.depthTestCheck, SIGNAL(toggled(bool)), canvas, SLOT(setOption(bool)));
+  connect(ui.ditherCheck, SIGNAL(toggled(bool)), canvas, SLOT(setOption(bool)));
+  connect(ui.backfaceCheck, SIGNAL(toggled(bool)), canvas, SLOT(setOption(bool)));
+  // ^ HACK: Replace this with a PROPER ui (with a panel for OpenGL state)
+}
+
 }
