ce3774ac6b6916c59a0903958a40ac344fe156f2
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-03-28 05:00:47 -0400

    Implement ImplementationInfo
    - Mostly; I need to give it another once-over

--- BALLS/BALLS/model/gl/ImplementationInfo.cpp
+++ BALLS/BALLS/model/gl/ImplementationInfo.cpp
@@ -1,11 +1,19 @@
 #include "precompiled.hpp"
 #include "model/gl/ImplementationInfo.hpp"
 
+#include <QOpenGLFunctions_3_0>
+
+#include "gl/OpenGLPointers.hpp"
+
 namespace balls {
 
-ImplementationInfo::ImplementationInfo()
-{
+ImplementationInfo::ImplementationInfo(OpenGLPointers& gl, QObject* parent)
+  : QObject(parent) {
+  Q_ASSUME(gl.gl30 != nullptr);
 
+  m_glslVersion = QString(gl.gl30->glGetString(GL_SHADING_LANGUAGE_VERSION));
+  m_renderer = QString(gl.gl30->glGetString(GL_RENDERER));
+  m_vendor = QString(gl.gl30->glGetString(GL_VENDOR));
+  m_version = QString(gl.gl30->glGetString(GL_VERSION));
 }
 }
-
--- BALLS/BALLS/model/gl/ImplementationInfo.hpp
+++ BALLS/BALLS/model/gl/ImplementationInfo.hpp
@@ -5,19 +5,24 @@
 
 namespace balls {
 
+class OpenGLPointers;
+
 class ImplementationInfo : public QObject {
   Q_OBJECT
 
-  Q_PROPERTY(QString glslVersion READ glslVersion CONSTANT FINAL)
-  Q_PROPERTY(QString renderer READ renderer CONSTANT FINAL)
-  Q_PROPERTY(QString vendor READ vendor CONSTANT FINAL)
-  Q_PROPERTY(QString version READ version CONSTANT FINAL)
+  Q_PROPERTY(QString glslVersion MEMBER m_glslVersion CONSTANT FINAL)
+  Q_PROPERTY(QString renderer MEMBER m_renderer CONSTANT FINAL)
+  Q_PROPERTY(QString vendor MEMBER m_vendor CONSTANT FINAL)
+  Q_PROPERTY(QString version MEMBER m_version CONSTANT FINAL)
 public:
-  ImplementationInfo();
+  ImplementationInfo(OpenGLPointers&, QObject* = nullptr);
 
-signals:
-
-public slots:
+private /* fields */:
+  QString m_glslVersion;
+  QString m_renderer;
+  QString m_vendor;
+  QString m_version;
+  // TODO: Get the extensions (glGet number of extensions, then glGetString them)
 };
 }
 
