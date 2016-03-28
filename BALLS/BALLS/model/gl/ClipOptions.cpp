d53a699022680d5351288de2fda2a0e07e069218
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-03-28 09:45:43 -0400

    Implement ClipOptions for the most part
    - May need to give it a once-over

--- BALLS/BALLS/model/gl/ClipOptions.cpp
+++ BALLS/BALLS/model/gl/ClipOptions.cpp
@@ -1,7 +1,82 @@
-#include "ViewOptions.hpp"
+#include "precompiled.hpp"
+#include "model/gl/ClipOptions.hpp"
 
-ViewOptions::ViewOptions(QObject *parent) : QObject(parent)
-{
+#include <QOpenGLFunctions_3_0>
 
+namespace balls {
+
+using std::array;
+
+ClipOptions::ClipOptions(OpenGLPointers& gl, QObject* parent)
+  : QObject(parent),
+    m_gl(gl),
+    m_viewportPosition(0, 0),
+    m_scissorPosition(0, 0),
+    m_scissorEnabled(false),
+    m_origin(ClipControlOrigin::LowerLeft),
+    m_depth(ClipControlDepth::NegativeOneToOne) {
+  Q_ASSUME(m_gl.gl30 != nullptr);
 }
 
+uvec2 ClipOptions::viewportSize() noexcept {
+  array<GLsizei, 4> viewport;
+  m_gl.gl30->glGetIntegerv(GL_VIEWPORT, viewport.data());
+
+  return {viewport[2], viewport[3]};
+}
+
+uvec2 ClipOptions::scissorSize() noexcept {
+  array<GLsizei, 4> scissor;
+  m_gl.gl30->glGetIntegerv(GL_SCISSOR_BOX, scissor.data());
+
+  return {scissor[2], scissor[3]};
+}
+
+void ClipOptions::setViewportPosition(const ivec2& position) noexcept {
+  m_viewportPosition = position;
+
+  uvec2 size = viewportSize();
+
+  m_gl.gl30->glViewport(position.x, position.y, size.x, size.y);
+}
+
+void ClipOptions::setViewportSize(const uvec2& size) noexcept {
+  m_gl.gl30->glViewport(
+    m_viewportPosition.x, m_viewportPosition.y, size.x, size.y);
+}
+
+void ClipOptions::setScissorPosition(const ivec2& position) noexcept {
+  m_scissorPosition = position;
+
+  uvec2 size = scissorSize();
+
+  m_gl.gl30->glScissor(position.x, position.y, size.x, size.y);
+}
+
+void ClipOptions::setScissorSize(const uvec2& size) noexcept {
+  m_gl.gl30->glScissor(
+    m_scissorPosition.x, m_scissorPosition.y, size.x, size.y);
+}
+
+void ClipOptions::setScissorEnabled(bool enabled) noexcept {
+  m_scissorEnabled = enabled;
+
+  if (m_scissorEnabled) {
+    m_gl.gl30->glEnable(GL_SCISSOR_TEST);
+  } else {
+    m_gl.gl30->glDisable(GL_SCISSOR_TEST);
+  }
+}
+
+void ClipOptions::setOrigin(ClipControlOrigin origin) noexcept {
+  m_origin = origin;
+
+  glClipControl(m_origin, m_depth);
+}
+
+void ClipOptions::setDepth(ClipControlDepth depth) noexcept {
+  m_depth = depth;
+
+  glClipControl(m_origin, m_depth);
+}
+}
--- BALLS/BALLS/model/gl/ClipOptions.hpp
+++ BALLS/BALLS/model/gl/ClipOptions.hpp
@@ -1,16 +1,71 @@
-#ifndef VIEWOPTIONS_HPP
-#define VIEWOPTIONS_HPP
+#ifndef CLIPOPTIONS_HPP
+#define CLIPOPTIONS_HPP
 
+#include <qopengl.h>
+#include <QObject>
 
-class ViewOptions : public QObject
-{
+#include <glm/vec2.hpp>
+
+#include "gl/OpenGLPointers.hpp"
+
+namespace balls {
+
+using glm::ivec2;
+using glm::vec2;
+using glm::uvec2;
+
+class ClipOptions : public QObject {
   Q_OBJECT
+
+  Q_ENUMS(ClipControlOrigin)
+  Q_ENUMS(ClipControlDepth)
+
+  // clang-format off
+  Q_PROPERTY(ivec2 viewportPosition MEMBER m_viewportPosition WRITE setViewportPosition FINAL)
+  Q_PROPERTY(uvec2 viewportSize READ viewportSize WRITE setViewportSize STORED false FINAL)
+  Q_PROPERTY(bool scissorEnabled MEMBER m_scissorEnabled WRITE setScissorEnabled FINAL)
+  Q_PROPERTY(ivec2 scissorPosition MEMBER m_scissorPosition WRITE setScissorPosition FINAL)
+  Q_PROPERTY(uvec2 scissorSize READ scissorSize WRITE setScissorSize STORED false FINAL)
+  Q_PROPERTY(ClipControlOrigin origin MEMBER m_origin WRITE setOrigin FINAL)
+  Q_PROPERTY(ClipControlDepth depth MEMBER m_depth WRITE setDepth FINAL)
+  // clang-format on
+
+public /* enums */:
+  enum ClipControlOrigin : GLenum {
+    LowerLeft = GL_LOWER_LEFT,
+    UpperLeft = GL_UPPER_LEFT,
+  };
+
+  enum ClipControlDepth : GLenum {
+    NegativeOneToOne = GL_NEGATIVE_ONE_TO_ONE,
+    ZeroToOne = GL_ZERO_TO_ONE,
+  };
+
 public:
-  explicit ViewOptions(QObject *parent = 0);
+  explicit ClipOptions(OpenGLPointers&, QObject* = nullptr);
 
-signals:
+private /* setters */:
+  uvec2 viewportSize() noexcept;
+  uvec2 scissorSize() noexcept;
 
-public slots:
+private /* setters */:
+  void setViewportPosition(const ivec2&) noexcept;
+  void setViewportSize(const uvec2&) noexcept;
+  void setScissorPosition(const ivec2&) noexcept;
+  void setScissorSize(const uvec2&) noexcept;
+  void setScissorEnabled(bool) noexcept;
+  void setOrigin(ClipControlOrigin) noexcept;
+  void setDepth(ClipControlDepth) noexcept;
+
+private /* members */:
+  OpenGLPointers m_gl;
+
+  bool m_scissorEnabled;
+  ivec2 m_viewportPosition;
+  ivec2 m_scissorPosition;
+  ClipControlOrigin m_origin;
+  ClipControlDepth m_depth;
 };
+}
 
-#endif // VIEWOPTIONS_HPP
+#endif // CLIPOPTIONS_HPP
