c2a9e88d4baa9757b2810b19455e19f36ef0eb81
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-04-01 15:43:58 -0400

    Flesh out StencilOptions

--- BALLS/BALLS/model/gl/StencilOptions.cpp
+++ BALLS/BALLS/model/gl/StencilOptions.cpp
@@ -1,7 +1,167 @@
-#include "StencilOptions.hpp"
+#include "precompiled.hpp"
+#include "model/gl/StencilOptions.hpp"
 
-StencilOptions::StencilOptions(QObject *parent) : QObject(parent)
-{
+#include <QOpenGLFunctions_3_0>
 
+namespace balls {
+
+StencilOptions::StencilOptions(OpenGLPointers& gl, QObject* parent)
+  : QObject(parent),
+    m_gl(gl),
+    m_stencilEnabled(false),
+    m_clearStencil(0),
+    m_frontStencilMask(0xFFFFFFFF),
+    m_backStencilMask(0xFFFFFFFF),
+    m_frontStencilFail(StencilAction::Keep),
+    m_frontStencilPassDepthFail(StencilAction::Keep),
+    m_frontStencilPassDepthPass(StencilAction::Keep),
+    m_backStencilFail(StencilAction::Keep),
+    m_backStencilPassDepthFail(StencilAction::Keep),
+    m_backStencilPassDepthPass(StencilAction::Keep),
+    m_frontStencilFunction(StencilFunction::Always),
+    m_frontStencilRef(0),
+    m_frontStencilValueMask(0xFFFFFFFF),
+    m_backStencilFunction(StencilFunction::Always),
+    m_backStencilRef(0),
+    m_backStencilValueMask(0xFFFFFFFF) {}
+
+
+void StencilOptions::setStencilEnabled(bool enabled) noexcept {
+  m_stencilEnabled = enabled;
+
+  if (m_stencilEnabled) {
+    m_gl.gl30->glEnable(GL_STENCIL_TEST);
+  } else {
+    m_gl.gl30->glDisable(GL_STENCIL_TEST);
+  }
 }
 
+void StencilOptions::setClearStencil(int clearStencil) noexcept {
+  m_clearStencil = clearStencil;
+
+  m_gl.gl30->glClearStencil(m_clearStencil);
+}
+
+void StencilOptions::setFrontStencilMask(unsigned int frontMask) noexcept {
+  m_frontStencilMask = frontMask;
+
+  m_gl.gl30->glStencilMaskSeparate(GL_FRONT, m_frontStencilMask);
+}
+
+void StencilOptions::setBackStencilMask(unsigned int backMask) noexcept {
+  m_backStencilMask = backMask;
+
+  m_gl.gl30->glStencilMaskSeparate(GL_BACK, m_backStencilMask);
+}
+
+void StencilOptions::setFrontStencilFail(StencilAction act) noexcept {
+  m_frontStencilFail = act;
+
+  m_gl.gl30->glStencilOpSeparate(
+    GL_FRONT,
+    m_frontStencilFail,
+    m_frontStencilPassDepthFail,
+    m_frontStencilPassDepthPass);
+}
+
+void StencilOptions::setFrontStencilPassDepthFail(StencilAction act) noexcept {
+  m_frontStencilPassDepthFail = act;
+
+  m_gl.gl30->glStencilOpSeparate(
+    GL_FRONT,
+    m_frontStencilFail,
+    m_frontStencilPassDepthFail,
+    m_frontStencilPassDepthPass);
+}
+
+void StencilOptions::setFrontStencilPassDepthPass(StencilAction act) noexcept {
+  m_frontStencilPassDepthPass = act;
+
+  m_gl.gl30->glStencilOpSeparate(
+    GL_FRONT,
+    m_frontStencilFail,
+    m_frontStencilPassDepthFail,
+    m_frontStencilPassDepthPass);
+}
+
+void StencilOptions::setBackStencilFail(StencilAction act) noexcept {
+  m_backStencilFail = act;
+
+  m_gl.gl30->glStencilOpSeparate(
+    GL_BACK,
+    m_backStencilFail,
+    m_backStencilPassDepthFail,
+    m_backStencilPassDepthPass);
+}
+
+void StencilOptions::setBackStencilPassDepthFail(StencilAction act) noexcept {
+  m_backStencilPassDepthFail = act;
+
+  m_gl.gl30->glStencilOpSeparate(
+    GL_BACK,
+    m_backStencilFail,
+    m_backStencilPassDepthFail,
+    m_backStencilPassDepthPass);
+}
+
+void StencilOptions::setBackStencilPassDepthPass(StencilAction act) noexcept {
+  m_backStencilPassDepthPass = act;
+
+  m_gl.gl30->glStencilOpSeparate(
+    GL_BACK,
+    m_backStencilFail,
+    m_backStencilPassDepthFail,
+    m_backStencilPassDepthPass);
+}
+
+void StencilOptions::setFrontStencilFunction(StencilFunction func) noexcept {
+  m_frontStencilFunction = func;
+
+  m_gl.gl30->glStencilFuncSeparate(
+    GL_FRONT,
+    m_frontStencilFunction,
+    m_frontStencilRef,
+    m_frontStencilValueMask);
+}
+
+void StencilOptions::setFrontStencilRef(int ref) noexcept {
+  m_frontStencilRef = ref;
+
+  m_gl.gl30->glStencilFuncSeparate(
+    GL_FRONT,
+    m_frontStencilFunction,
+    m_frontStencilRef,
+    m_frontStencilValueMask);
+}
+
+void StencilOptions::setFrontStencilValueMask(unsigned int mask) noexcept {
+  m_frontStencilValueMask = mask;
+
+  m_gl.gl30->glStencilFuncSeparate(
+    GL_FRONT,
+    m_frontStencilFunction,
+    m_frontStencilRef,
+    m_frontStencilValueMask);
+}
+
+void StencilOptions::setBackStencilFunction(StencilFunction func) noexcept {
+  m_backStencilFunction = func;
+
+  m_gl.gl30->glStencilFuncSeparate(
+    GL_BACK, m_backStencilFunction, m_backStencilRef, m_backStencilValueMask);
+}
+
+void StencilOptions::setBackStencilRef(int ref) noexcept {
+  m_backStencilRef = ref;
+
+  m_gl.gl30->glStencilFuncSeparate(
+    GL_BACK, m_backStencilFunction, m_backStencilRef, m_backStencilValueMask);
+}
+
+void StencilOptions::setBackStencilValueMask(unsigned int mask) noexcept {
+  m_backStencilValueMask = mask;
+
+  m_gl.gl30->glStencilFuncSeparate(
+    GL_BACK, m_backStencilFunction, m_backStencilRef, m_backStencilValueMask);
+}
+}
--- BALLS/BALLS/model/gl/StencilOptions.hpp
+++ BALLS/BALLS/model/gl/StencilOptions.hpp
@@ -1,16 +1,117 @@
 #ifndef STENCILOPTIONS_HPP
 #define STENCILOPTIONS_HPP
 
+#include <QObject>
 
-class StencilOptions : public QObject
-{
+#include <qopengl.h>
+
+#include "gl/OpenGLPointers.hpp"
+
+namespace balls {
+
+class StencilOptions : public QObject {
   Q_OBJECT
+
+  Q_ENUMS(StencilAction)
+  Q_ENUMS(StencilFunction)
+
+  // clang-format off
+  Q_PROPERTY(bool stencilEnabled MEMBER m_stencilEnabled WRITE setStencilEnabled FINAL)
+  Q_PROPERTY(int clearStencil MEMBER m_clearStencil WRITE setClearStencil FINAL)
+  Q_PROPERTY(uint frontStencilMask MEMBER m_frontStencilMask WRITE setFrontStencilMask FINAL)
+  Q_PROPERTY(uint backStencilMask MEMBER m_backStencilMask WRITE setBackStencilMask FINAL)
+
+  Q_PROPERTY(StencilAction frontStencilFail MEMBER m_frontStencilFail WRITE setFrontStencilFail FINAL)
+  Q_PROPERTY(StencilAction frontStencilPassDepthFail MEMBER m_frontStencilPassDepthFail WRITE setFrontStencilPassDepthFail FINAL)
+  Q_PROPERTY(StencilAction frontStencilPassDepthPass MEMBER m_frontStencilPassDepthPass WRITE setFrontStencilPassDepthPass FINAL)
+
+  Q_PROPERTY(StencilAction backStencilFail MEMBER m_backStencilFail WRITE setBackStencilFail FINAL)
+  Q_PROPERTY(StencilAction backStencilPassDepthFail MEMBER m_backStencilPassDepthFail WRITE setBackStencilPassDepthFail FINAL)
+  Q_PROPERTY(StencilAction backStencilPassDepthPass MEMBER m_backStencilPassDepthPass WRITE setBackStencilPassDepthPass FINAL)
+
+  Q_PROPERTY(StencilFunction frontStencilFunction MEMBER m_frontStencilFunction WRITE setFrontStencilFunction FINAL)
+  Q_PROPERTY(int frontStencilRef MEMBER m_frontStencilRef WRITE setFrontStencilRef FINAL)
+  Q_PROPERTY(uint frontStencilValueMask MEMBER m_frontStencilValueMask WRITE setFrontStencilValueMask FINAL)
+
+  Q_PROPERTY(StencilFunction backStencilFunction MEMBER m_backStencilFunction WRITE setBackStencilFunction FINAL)
+  Q_PROPERTY(int backStencilRef MEMBER m_backStencilRef WRITE setBackStencilRef FINAL)
+  Q_PROPERTY(uint backStencilValueMask MEMBER m_backStencilValueMask WRITE setBackStencilValueMask FINAL)
+  // clang-format on
+
+public /* enums */:
+  enum StencilAction : GLenum {
+    Keep = GL_KEEP,
+    Zero = GL_ZERO,
+    Replace = GL_REPLACE,
+    Increment = GL_INCR,
+    IncrementWrap = GL_INCR_WRAP,
+    Decrement = GL_DECR,
+    DecrementWrap = GL_DECR_WRAP,
+    Invert = GL_INVERT,
+  };
+
+  enum StencilFunction : GLenum {
+    Never = GL_NEVER,
+    Less = GL_LESS,
+    Equal = GL_EQUAL,
+    LessEqual = GL_LEQUAL,
+    Greater = GL_GREATER,
+    NotEqual = GL_NOTEQUAL,
+    GreaterEqual = GL_GEQUAL,
+    Always = GL_ALWAYS,
+  };
+
 public:
-  explicit StencilOptions(QObject *parent = 0);
+  explicit StencilOptions(OpenGLPointers&, QObject* = nullptr);
 
-signals:
+private /* setters */:
+  void setStencilEnabled(bool) noexcept;
+  void setClearStencil(int) noexcept;
+  void setFrontStencilMask(unsigned int) noexcept;
+  void setBackStencilMask(unsigned int) noexcept;
 
-public slots:
+  void setFrontStencilFail(StencilAction) noexcept;
+  void setFrontStencilPassDepthFail(StencilAction) noexcept;
+  void setFrontStencilPassDepthPass(StencilAction) noexcept;
+
+  void setBackStencilFail(StencilAction) noexcept;
+  void setBackStencilPassDepthFail(StencilAction) noexcept;
+  void setBackStencilPassDepthPass(StencilAction) noexcept;
+
+  void setFrontStencilFunction(StencilFunction) noexcept;
+  void setFrontStencilRef(int) noexcept;
+  void setFrontStencilValueMask(unsigned int) noexcept;
+
+  void setBackStencilFunction(StencilFunction) noexcept;
+  void setBackStencilRef(int) noexcept;
+  void setBackStencilValueMask(unsigned int) noexcept;
+
+private /* members */:
+  OpenGLPointers m_gl;
+
+  bool m_stencilEnabled;
+  int m_clearStencil;
+  unsigned int m_frontStencilMask;
+  unsigned int m_backStencilMask;
+
+  StencilAction m_frontStencilFail;
+  StencilAction m_frontStencilPassDepthFail;
+  StencilAction m_frontStencilPassDepthPass;
+
+  StencilAction m_backStencilFail;
+  StencilAction m_backStencilPassDepthFail;
+  StencilAction m_backStencilPassDepthPass;
+
+  StencilFunction m_frontStencilFunction;
+  int m_frontStencilRef;
+  unsigned int m_frontStencilValueMask;
+
+  StencilFunction m_backStencilFunction;
+  int m_backStencilRef;
+  unsigned int m_backStencilValueMask;
 };
+}
+
+Q_DECLARE_METATYPE(balls::StencilOptions*)
 
 #endif // STENCILOPTIONS_HPP
