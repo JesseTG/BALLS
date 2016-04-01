64664ae4fa3c423277ec882b67abf8e062589f81
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-04-01 15:43:52 -0400

    Flesh out SampleOptions

--- BALLS/BALLS/model/gl/SampleOptions.cpp
+++ BALLS/BALLS/model/gl/SampleOptions.cpp
@@ -1,7 +1,101 @@
 #include "precompiled.hpp"
 #include "model/gl/SampleOptions.hpp"
 
+#include <QOpenGLFunctions_3_0>
+#include <QOpenGLFunctions_4_0_Core>
+
 namespace balls {
 
-SampleOptions::SampleOptions(QObject *parent) : QObject(parent) {}
+SampleOptions::SampleOptions(OpenGLPointers& gl, QObject* parent)
+  : QObject(parent),
+    m_gl(gl),
+    m_sampleCoverage(1.0f),
+    m_invertSampleCoverage(false),
+    m_sampleAlphaToCoverage(false),
+    m_sampleAlphaToOne(false),
+    m_sampleCoverageEnabled(false),
+    m_sampleShadingEnabled(false),
+    m_sampleMaskEnabled(false),
+    m_multisample(true),
+    m_minSampleShading(1.0f) {}
+
+void SampleOptions::setSampleCoverage(float sampleCoverage) noexcept {
+  m_sampleCoverage = sampleCoverage;
+
+  m_gl.gl30->glSampleCoverage(m_sampleCoverage, m_invertSampleCoverage);
+}
+
+void SampleOptions::setInvertSampleCoverage(
+  bool invertSampleCoverage) noexcept {
+  m_invertSampleCoverage = invertSampleCoverage;
+
+  m_gl.gl30->glSampleCoverage(m_sampleCoverage, m_invertSampleCoverage);
+}
+
+void SampleOptions::setSampleAlphaToCoverage(bool coverage) noexcept {
+  m_sampleAlphaToCoverage = coverage;
+
+  if (m_sampleAlphaToCoverage) {
+    m_gl.gl30->glEnable(GL_SAMPLE_ALPHA_TO_COVERAGE);
+  } else {
+    m_gl.gl30->glDisable(GL_SAMPLE_ALPHA_TO_COVERAGE);
+  }
+}
+
+void SampleOptions::setSampleAlphaToOne(bool sampleAlphaToOne) noexcept {
+  m_sampleAlphaToOne = sampleAlphaToOne;
+
+  if (m_sampleAlphaToOne) {
+    m_gl.gl30->glEnable(GL_SAMPLE_ALPHA_TO_ONE);
+  } else {
+    m_gl.gl30->glDisable(GL_SAMPLE_ALPHA_TO_ONE);
+  }
+}
+void SampleOptions::setSampleCoverageEnabled(bool sampleCoverage) noexcept {
+  m_sampleCoverage = sampleCoverage;
+
+  if (m_sampleCoverage) {
+    m_gl.gl30->glEnable(GL_SAMPLE_COVERAGE);
+  } else {
+    m_gl.gl30->glDisable(GL_SAMPLE_COVERAGE);
+  }
+}
+
+void SampleOptions::setSampleShadingEnabled(bool enabled) noexcept {
+  m_sampleShadingEnabled = enabled;
+
+  if (m_sampleShadingEnabled) {
+    m_gl.gl30->glEnable(GL_SAMPLE_SHADING);
+  } else {
+    m_gl.gl30->glDisable(GL_SAMPLE_SHADING);
+  }
+}
+
+void SampleOptions::setSampleMaskEnabled(bool sampleMask) noexcept {
+  m_sampleMaskEnabled = sampleMask;
+
+  if (m_sampleMaskEnabled) {
+    m_gl.gl30->glEnable(GL_SAMPLE_MASK);
+  } else {
+    m_gl.gl30->glDisable(GL_SAMPLE_MASK);
+  }
+}
+
+void SampleOptions::setMultisample(bool multisample) noexcept {
+  m_multisample = multisample;
+
+  if (m_multisample) {
+    m_gl.gl30->glEnable(GL_MULTISAMPLE);
+  } else {
+    m_gl.gl30->glDisable(GL_MULTISAMPLE);
+  }
+}
+void SampleOptions::setMinSampleShading(float minSampleShading) noexcept {
+  m_minSampleShading = minSampleShading;
+
+  if (m_gl.gl40) {
+    // If OpenGL 4.0 is available...
+    m_gl.gl40->glMinSampleShading(m_minSampleShading);
+  }
+}
 }
--- BALLS/BALLS/model/gl/SampleOptions.hpp
+++ BALLS/BALLS/model/gl/SampleOptions.hpp
@@ -3,6 +3,8 @@
 
 #include <QObject>
 
+#include "gl/OpenGLPointers.hpp"
+
 namespace balls {
 
 class SampleOptions : public QObject {
@@ -11,15 +13,41 @@ class SampleOptions : public QObject {
   // clang-format off
   Q_PROPERTY(float sampleCoverage MEMBER m_sampleCoverage WRITE setSampleCoverage FINAL)
   Q_PROPERTY(bool invertSampleCoverage MEMBER m_invertSampleCoverage WRITE setInvertSampleCoverage FINAL)
+  Q_PROPERTY(bool sampleAlphaToCoverage MEMBER m_sampleAlphaToCoverage WRITE setSampleAlphaToCoverage FINAL)
+  Q_PROPERTY(bool sampleAlphaToOne MEMBER m_sampleAlphaToOne WRITE setSampleAlphaToOne FINAL)
+  Q_PROPERTY(bool sampleCoverageEnabled MEMBER m_sampleCoverageEnabled WRITE setSampleCoverageEnabled FINAL)
+  Q_PROPERTY(bool sampleShadingEnabled MEMBER m_sampleShadingEnabled WRITE setSampleShadingEnabled FINAL)
+  Q_PROPERTY(bool sampleMaskEnabled MEMBER m_sampleMaskEnabled WRITE setSampleMaskEnabled FINAL)
+  Q_PROPERTY(bool multisample MEMBER m_multisample WRITE setMultisample FINAL)
+  Q_PROPERTY(float minSampleShading MEMBER m_minSampleShading WRITE setMinSampleShading FINAL)
   // clang-format on
 
 public:
-  explicit SampleOptions(QObject *parent = 0);
+  explicit SampleOptions(OpenGLPointers&, QObject* = nullptr);
 
 private /* setters */:
+  void setSampleCoverage(float) noexcept;
+  void setInvertSampleCoverage(bool) noexcept;
+  void setSampleAlphaToCoverage(bool) noexcept;
+  void setSampleAlphaToOne(bool) noexcept;
+  void setSampleCoverageEnabled(bool) noexcept;
+  void setSampleShadingEnabled(bool) noexcept;
+  void setSampleMaskEnabled(bool) noexcept;
+  void setMultisample(bool) noexcept;
+  void setMinSampleShading(float) noexcept;
+
 private /* fields */:
+  OpenGLPointers m_gl;
+
   float m_sampleCoverage;
   bool m_invertSampleCoverage;
+  bool m_sampleAlphaToCoverage;
+  bool m_sampleAlphaToOne;
+  bool m_sampleCoverageEnabled;
+  bool m_sampleShadingEnabled;
+  bool m_sampleMaskEnabled;
+  bool m_multisample;
+  float m_minSampleShading;
 };
 }
 
