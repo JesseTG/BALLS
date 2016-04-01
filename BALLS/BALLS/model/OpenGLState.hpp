2f7b9c42d57f5b0f0f327d741ee8523ef05a6135
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-04-01 15:45:25 -0400

    Clean up OpenGLState

--- BALLS/BALLS/model/OpenGLState.hpp
+++ BALLS/BALLS/model/OpenGLState.hpp
@@ -21,69 +21,18 @@ class OpenGLState : public QObject {
    * - Implementation info (DONE), except extensions
    * - Limits (GL_MAX_VARYING_COMPONENTS, etc.)
    * - Hints (DONE)
-   * - Stencil
+   * - Stencil (DONE)
    * - Depth (DONE)
    * - Color (DONE)
-   * - Geometry
-   * - Sampling
-   * - View (scissor, viewport)
+   * - Geometry (DONE)
+   * - Sampling (DONE)
+   * - View (scissor, viewport) (DONE)
    *
    */
 
-
-  Q_ENUMS(CullFace)
-  Q_ENUMS(FrontFace)
-  Q_ENUMS(PolygonMode)
-  Q_ENUMS(ProvokeMode)
-  Q_ENUMS(SpriteCoordOrigin)
-
-  // clang-format off
-  Q_PROPERTY(int clearStencil READ clearSTencil WRITE setClearStencil STORED false FINAL)
-  Q_PROPERTY(CullFace cullFace READ cullFace WRITE setCullFace STORED false FINAL)
-  Q_PROPERTY(FrontFace frontFace READ frontFace WRITE setFrontFace STORED false FINAL)
-  Q_PROPERTY(float lineWidth READ lineWidth WRITE setLineWidth STORED false FINAL)
-  Q_PROPERTY(float pointFadeThresholdSize READ pointFadeThresholdSize WRITE setPointFadeThresholdSize STORED false FINAL)
-  Q_PROPERTY(float pointSize READ pointSize WRITE setPointSize STORED false FINAL)
-  Q_PROPERTY(PolygonMode polygonMode READ polygonMode WRITE setPolygonMode STORED false FINAL)
-  Q_PROPERTY(float polygonOffsetFactor READ polygonOffsetFactor WRITE setPolygonOffsetFactor STORED false FINAL)
-  Q_PROPERTY(float polygonOffsetUnits READ polygonOffsetUnits WRITE setPolygonOffsetUnits STORED false FINAL)
-  Q_PROPERTY(uint primitiveRestartIndex READ primitiveRestartIndex WRITE setPrimitiveRestartIndex STORED false FINAL)
-  Q_PROPERTY(ProvokeMode provokingVertex READ provokingVertex WRITE setProvokingIndex STORED false FINAL)
-  Q_PROPERTY(SpriteCoordOrigin spriteCoordOrigin READ spriteCoordOrigin WRITE setSpriteCoordOrigin STORED false FINAL)
-
-  // clang-format on
-
-public /* enums */:
-
-
-  enum CullFace {
-    None,
-    Front = GL_FRONT,
-    Back = GL_BACK,
-    FrontAndBack = GL_FRONT_AND_BACK,
-  };
-
-  enum FrontFace {
-    Clockwise = GL_CW,
-    CounterClockwise = GL_CCW,
-  };
-
-
-  enum PolygonMode {
-    Point = GL_POINT,
-    Line = GL_LINE,
-    Fill = GL_FILL,
-  };
-
-  enum ProvokeMode {
-    FirstVertexConvention = GL_FIRST_VERTEX_CONVENTION,
-    LastVertexConvention = GL_LAST_VERTEX_CONVENTION,
-  };
-
-  enum SpriteCoordOrigin {
-    UpperLeft = GL_UPPER_LEFT,
-    LowerLeft = GL_LOWER_LEFT,
-  };
+  // GL_RASTERIZER_DISCARD
+  // GL_CLIP_DISTANCE
+  // GL_TEXTURE_CUBE_MAP_SEAMLESS
 
 public:
   explicit OpenGLState(QOpenGLContext*, QObject* = nullptr);
