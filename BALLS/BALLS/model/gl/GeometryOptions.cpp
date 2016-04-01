756fc5cd09367160cef6139a577dab9e016cdc6b
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-04-01 15:43:41 -0400

    Flesh out GeometryOptions

--- BALLS/BALLS/model/gl/GeometryOptions.cpp
+++ BALLS/BALLS/model/gl/GeometryOptions.cpp
@@ -1,7 +1,166 @@
-#include "GeometryOptions.hpp"
+#include "precompiled.hpp"
+#include "model/gl/GeometryOptions.hpp"
 
-GeometryOptions::GeometryOptions(QObject *parent) : QObject(parent)
-{
+#include <QOpenGLFunctions_3_0>
+#include <QOpenGLFunctions_3_2_Core>
 
+namespace balls {
+
+GeometryOptions::GeometryOptions(OpenGLPointers& gl, QObject* parent)
+  : QObject(parent),
+    m_gl(gl),
+    m_cullFace(CullFace::None),
+    m_frontFace(FrontFace::CounterClockwise),
+    m_polygonMode(PolygonMode::Fill),
+    m_provokingVertex(ProvokeMode::LastVertexConvention),
+    m_primitiveRestartEnabled(false),
+    m_primitiveRestartIndex(0),
+    m_programPointSize(false),
+    m_pointFadeThresholdSize(1.0f),
+    m_pointSize(1.0f),
+    m_lineWidth(1.0f),
+    m_polygonOffsetFactor(0.0f),
+    m_polygonOffsetUnits(0.0f),
+    m_polygonOffsetFill(false),
+    m_polygonOffsetPoint(false),
+    m_polygonOffsetLine(false),
+    m_polygonSmooth(false),
+    m_spriteCoordOrigin(SpriteCoordOrigin::UpperLeft) {}
+// Initialized with the defualt values as specified by OpenGL
+
+void GeometryOptions::setCullFace(CullFace cullFace) noexcept {
+  m_cullFace = cullFace;
+
+  if (m_cullFace == CullFace::None) {
+    // If we're turning off backface culling...
+
+    m_gl.gl30->glDisable(GL_CULL_FACE);
+  } else {
+    m_gl.gl30->glEnable(GL_CULL_FACE);
+    m_gl.gl30->glCullFace(m_cullFace);
+  }
 }
 
+void GeometryOptions::setFrontFace(FrontFace frontFace) noexcept {
+  m_frontFace = frontFace;
+
+  m_gl.gl30->glFrontFace(m_frontFace);
+}
+
+void GeometryOptions::setLineWidth(float lineWidth) noexcept {
+  m_lineWidth = lineWidth;
+
+  m_gl.gl30->glLineWidth(m_lineWidth);
+}
+
+void GeometryOptions::setPointFadeThresholdSize(float threshold) noexcept {
+  m_pointFadeThresholdSize = threshold;
+
+  m_gl.gl30->glPointParameterf(
+    GL_POINT_FADE_THRESHOLD_SIZE, m_pointFadeThresholdSize);
+}
+
+void GeometryOptions::setPointSize(float pointSize) noexcept {
+  m_pointSize = pointSize;
+
+  m_gl.gl30->glPointSize(pointSize);
+}
+
+void GeometryOptions::setPolygonMode(PolygonMode polygonMode) noexcept {
+  m_polygonMode = polygonMode;
+
+  m_gl.gl30->glPolygonMode(GL_FRONT_AND_BACK, polygonMode);
+}
+
+void GeometryOptions::setPolygonOffsetFactor(float factor) noexcept {
+  m_polygonOffsetFactor = factor;
+
+  m_gl.gl30->glPolygonOffset(m_polygonOffsetFactor, m_polygonOffsetUnits);
+}
+
+void GeometryOptions::setPolygonOffsetUnits(float units) noexcept {
+  m_polygonOffsetUnits = units;
+
+  m_gl.gl30->glPolygonOffset(m_polygonOffsetFactor, m_polygonOffsetUnits);
+}
+void GeometryOptions::setPolygonOffsetFill(bool fill) noexcept {
+  m_polygonOffsetFill = fill;
+
+  if (m_polygonOffsetFill) {
+    m_gl.gl30->glEnable(GL_POLYGON_OFFSET_FILL);
+  } else {
+    m_gl.gl30->glDisable(GL_POLYGON_OFFSET_FILL);
+  }
+}
+void GeometryOptions::setPolygonOffsetPoint(bool point) noexcept {
+  m_polygonOffsetPoint = point;
+
+  if (m_polygonOffsetPoint) {
+    m_gl.gl30->glEnable(GL_POLYGON_OFFSET_POINT);
+  } else {
+    m_gl.gl30->glDisable(GL_POLYGON_OFFSET_POINT);
+  }
+}
+void GeometryOptions::setPolygonOffsetLine(bool line) noexcept {
+  m_polygonOffsetLine = line;
+
+  if (m_polygonOffsetLine) {
+    m_gl.gl30->glEnable(GL_POLYGON_OFFSET_LINE);
+  } else {
+    m_gl.gl30->glDisable(GL_POLYGON_OFFSET_LINE);
+  }
+}
+
+void GeometryOptions::setPrimitiveRestartEnabled(bool enabled) noexcept {
+  m_primitiveRestartEnabled = enabled;
+
+  if (m_primitiveRestartEnabled) {
+    m_gl.gl30->glEnable(GL_PRIMITIVE_RESTART);
+  } else {
+    m_gl.gl30->glDisable(GL_PRIMITIVE_RESTART);
+  }
+}
+
+void GeometryOptions::setPrimitiveRestartIndex(unsigned int index) noexcept {
+  m_primitiveRestartIndex = index;
+
+  if (m_gl.gl32) {
+    // If OpenGL 3.2 is available...
+    m_gl.gl32->glPrimitiveRestartIndex(m_primitiveRestartIndex);
+  }
+}
+
+void GeometryOptions::setProvokingVertex(ProvokeMode provokeMode) noexcept {
+  m_provokingVertex = provokeMode;
+
+  if (m_gl.gl32) {
+    // If OpenGL 3.2 is available...
+    m_gl.gl32->glProvokingVertex(m_provokingVertex);
+  }
+}
+void GeometryOptions::setProgramPointSize(bool enabled) noexcept {
+  m_programPointSize = enabled;
+
+  if (m_programPointSize) {
+    m_gl.gl30->glEnable(GL_PROGRAM_POINT_SIZE);
+  } else {
+    m_gl.gl30->glDisable(GL_PROGRAM_POINT_SIZE);
+  }
+}
+void GeometryOptions::setPolygonSmooth(bool enabled) noexcept {
+  m_polygonSmooth = enabled;
+
+  if (m_polygonSmooth) {
+    m_gl.gl30->glEnable(GL_POLYGON_SMOOTH);
+  } else {
+    m_gl.gl30->glDisable(GL_POLYGON_SMOOTH);
+  }
+}
+
+void GeometryOptions::setSpriteCoordOrigin(SpriteCoordOrigin origin) noexcept {
+  m_spriteCoordOrigin = origin;
+
+  m_gl.gl30->glPointParameteri(
+    GL_POINT_SPRITE_COORD_ORIGIN, m_spriteCoordOrigin);
+}
+}
--- BALLS/BALLS/model/gl/GeometryOptions.hpp
+++ BALLS/BALLS/model/gl/GeometryOptions.hpp
@@ -1,16 +1,119 @@
 #ifndef GEOMETRYOPTIONS_HPP
 #define GEOMETRYOPTIONS_HPP
 
+#include <qopengl.h>
+#include <QObject>
 
-class GeometryOptions : public QObject
-{
+#include "gl/OpenGLPointers.hpp"
+
+namespace balls {
+
+class GeometryOptions : public QObject {
   Q_OBJECT
+
+  Q_ENUMS(CullFace)
+  Q_ENUMS(FrontFace)
+  Q_ENUMS(PolygonMode)
+  Q_ENUMS(ProvokeMode)
+  Q_ENUMS(SpriteCoordOrigin)
+
+  // clang-format off
+  Q_PROPERTY(CullFace cullFace MEMBER m_cullFace WRITE setCullFace FINAL)
+  Q_PROPERTY(FrontFace frontFace MEMBER m_frontFace WRITE setFrontFace FINAL)
+  Q_PROPERTY(float lineWidth MEMBER m_lineWidth WRITE setLineWidth FINAL)
+  Q_PROPERTY(float pointFadeThresholdSize MEMBER m_pointFadeThresholdSize WRITE setPointFadeThresholdSize FINAL)
+  Q_PROPERTY(float pointSize MEMBER m_pointSize WRITE setPointSize FINAL)
+  Q_PROPERTY(PolygonMode polygonMode MEMBER m_polygonMode WRITE setPolygonMode FINAL)
+  Q_PROPERTY(float polygonOffsetFactor MEMBER m_polygonOffsetFactor WRITE setPolygonOffsetFactor FINAL)
+  Q_PROPERTY(float polygonOffsetUnits MEMBER m_polygonOffsetUnits WRITE setPolygonOffsetUnits FINAL)
+  Q_PROPERTY(bool polygonOffsetFill MEMBER m_polygonOffsetFill WRITE setPolygonOffsetFill FINAL)
+  Q_PROPERTY(bool polygonOffsetPoint MEMBER m_polygonOffsetPoint WRITE setPolygonOffsetPoint FINAL)
+  Q_PROPERTY(bool polygonOffsetLine MEMBER m_polygonOffsetLine WRITE setPolygonOffsetLine FINAL)
+  Q_PROPERTY(bool primitiveRestartEnabled MEMBER m_primitiveRestartEnabled WRITE setPrimitiveRestartEnabled FINAL)
+  Q_PROPERTY(uint primitiveRestartIndex MEMBER m_primitiveRestartIndex WRITE setPrimitiveRestartIndex FINAL)
+  Q_PROPERTY(ProvokeMode provokingVertex MEMBER m_provokingVertex WRITE setProvokingVertex FINAL)
+  Q_PROPERTY(bool programPointSize MEMBER m_programPointSize WRITE setProgramPointSize FINAL)
+  Q_PROPERTY(bool polygonSmooth MEMBER m_polygonSmooth WRITE setPolygonSmooth FINAL)
+  Q_PROPERTY(SpriteCoordOrigin spriteCoordOrigin MEMBER m_spriteCoordOrigin WRITE setSpriteCoordOrigin FINAL)
+
+  // clang-format on
+
+public /* enums */:
+  enum CullFace : GLenum {
+    None,
+    Front = GL_FRONT,
+    Back = GL_BACK,
+    FrontAndBack = GL_FRONT_AND_BACK,
+  };
+
+  enum FrontFace : GLenum {
+    Clockwise = GL_CW,
+    CounterClockwise = GL_CCW,
+  };
+
+
+  enum PolygonMode : GLenum {
+    Point = GL_POINT,
+    Line = GL_LINE,
+    Fill = GL_FILL,
+  };
+
+  enum ProvokeMode : GLenum {
+    FirstVertexConvention = GL_FIRST_VERTEX_CONVENTION,
+    LastVertexConvention = GL_LAST_VERTEX_CONVENTION,
+  };
+
+  enum SpriteCoordOrigin {
+    UpperLeft = GL_UPPER_LEFT,
+    LowerLeft = GL_LOWER_LEFT,
+  };
+
 public:
-  explicit GeometryOptions(QObject *parent = 0);
+  explicit GeometryOptions(OpenGLPointers&, QObject* = nullptr);
 
-signals:
+private /* setters */:
+  void setCullFace(CullFace) noexcept;
+  void setFrontFace(FrontFace) noexcept;
+  void setLineWidth(float) noexcept;
+  void setPointFadeThresholdSize(float) noexcept;
+  void setPointSize(float) noexcept;
+  void setPolygonMode(PolygonMode) noexcept;
+  void setPolygonOffsetFactor(float) noexcept;
+  void setPolygonOffsetUnits(float) noexcept;
+  void setPolygonOffsetFill(bool) noexcept;
+  void setPolygonOffsetPoint(bool) noexcept;
+  void setPolygonOffsetLine(bool) noexcept;
+  void setPrimitiveRestartEnabled(bool) noexcept;
+  void setPrimitiveRestartIndex(unsigned int) noexcept;
+  void setProvokingVertex(ProvokeMode) noexcept;
+  void setProgramPointSize(bool) noexcept;
+  void setPolygonSmooth(bool) noexcept;
+  void setSpriteCoordOrigin(SpriteCoordOrigin) noexcept;
 
-public slots:
+private /* members */:
+  OpenGLPointers m_gl;
+
+  CullFace m_cullFace;
+  FrontFace m_frontFace;
+  PolygonMode m_polygonMode;
+  ProvokeMode m_provokingVertex;
+  bool m_primitiveRestartEnabled;
+  unsigned int m_primitiveRestartIndex;
+  bool m_programPointSize;
+  float m_pointFadeThresholdSize;
+  float m_pointSize;
+  float m_lineWidth;
+  float m_polygonOffsetFactor;
+  float m_polygonOffsetUnits;
+  bool m_polygonOffsetFill;
+  bool m_polygonOffsetPoint;
+  bool m_polygonOffsetLine;
+  bool m_polygonSmooth;
+  SpriteCoordOrigin m_spriteCoordOrigin;
 };
+}
+
+
+Q_DECLARE_METATYPE(balls::GeometryOptions*)
 
 #endif // GEOMETRYOPTIONS_HPP
