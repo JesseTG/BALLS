b8ca1f96322c89bdc054d818ad500d97604bac29
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-04-01 16:24:18 -0400

    Adjusted initialization of OpenGLStateWidget
    - All members are now pointers and are initialized later

--- BALLS/BALLS/ui/docks/OpenGLStateWidget.cpp
+++ BALLS/BALLS/ui/docks/OpenGLStateWidget.cpp
@@ -1,31 +1,54 @@
 #include "precompiled.hpp"
 #include "ui/docks/OpenGLStateWidget.hpp"
 
-#include "gl/OpenGLPointers.hpp"
+#include "model/gl/BlendState.hpp"
+#include "model/gl/ClipOptions.hpp"
+#include "model/gl/ColorOptions.hpp"
+#include "model/gl/DepthOptions.hpp"
+#include "model/gl/GeometryOptions.hpp"
+#include "model/gl/Hints.hpp"
+#include "model/gl/ImplementationInfo.hpp"
+#include "model/gl/SampleOptions.hpp"
+#include "model/gl/StencilOptions.hpp"
 
 namespace balls {
 
-OpenGLStateWidget::OpenGLStateWidget(OpenGLPointers& gl, QWidget* parent)
+OpenGLStateWidget::OpenGLStateWidget(QWidget* parent)
   : QWidget(parent),
-    m_blendState(gl),
-    m_clipOptions(gl),
-    m_colorOptions(gl),
-    m_depthOptions(gl),
-    m_geometryOptions(gl),
-    m_hints(gl),
-    m_implementationInfo(gl),
-    m_sampleOptions(gl),
-    m_stencilOptions(gl) {
+    m_blendState(nullptr),
+    m_clipOptions(nullptr),
+    m_colorOptions(nullptr),
+    m_depthOptions(nullptr),
+    m_geometryOptions(nullptr),
+    m_hints(nullptr),
+    m_implementationInfo(nullptr),
+    m_sampleOptions(nullptr),
+    m_stencilOptions(nullptr) {
   ui.setupUi(this);
+}
 
-  ui.openGlProperties->addObject(&m_blendState);
-  ui.openGlProperties->addObject(&m_clipOptions);
-  ui.openGlProperties->addObject(&m_colorOptions);
-  ui.openGlProperties->addObject(&m_depthOptions);
-  ui.openGlProperties->addObject(&m_geometryOptions);
-  ui.openGlProperties->addObject(&m_hints);
-  ui.openGlProperties->addObject(&m_implementationInfo);
-  ui.openGlProperties->addObject(&m_sampleOptions);
-  ui.openGlProperties->addObject(&m_stencilOptions);
+
+void OpenGLStateWidget::setOpenGLPointers(const OpenGLPointers& gl) noexcept {
+  m_gl = gl;
+
+  m_blendState = new BlendState(m_gl, this);
+  m_clipOptions = new ClipOptions(m_gl, this);
+  m_colorOptions = new ColorOptions(m_gl, this);
+  m_depthOptions = new DepthOptions(m_gl, this);
+  m_geometryOptions = new GeometryOptions(m_gl, this);
+  m_hints = new Hints(m_gl, this);
+  m_implementationInfo = new ImplementationInfo(m_gl, this);
+  m_sampleOptions = new SampleOptions(m_gl, this);
+  m_stencilOptions = new StencilOptions(m_gl, this);
+
+  ui.openGlProperties->addObject(m_blendState);
+  ui.openGlProperties->addObject(m_clipOptions);
+  ui.openGlProperties->addObject(m_colorOptions);
+  ui.openGlProperties->addObject(m_depthOptions);
+  ui.openGlProperties->addObject(m_geometryOptions);
+  ui.openGlProperties->addObject(m_hints);
+  ui.openGlProperties->addObject(m_implementationInfo);
+  ui.openGlProperties->addObject(m_sampleOptions);
+  ui.openGlProperties->addObject(m_stencilOptions);
 }
 }
--- BALLS/BALLS/ui/docks/OpenGLStateWidget.hpp
+++ BALLS/BALLS/ui/docks/OpenGLStateWidget.hpp
@@ -2,38 +2,43 @@
 #define OPENGLSTATEWIDGET_HPP
 
 #include "ui_OpenGLStateWidget.h"
-#include "model/gl/BlendState.hpp"
-#include "model/gl/ClipOptions.hpp"
-#include "model/gl/ColorOptions.hpp"
-#include "model/gl/DepthOptions.hpp"
-#include "model/gl/GeometryOptions.hpp"
-#include "model/gl/Hints.hpp"
-#include "model/gl/ImplementationInfo.hpp"
-#include "model/gl/SampleOptions.hpp"
-#include "model/gl/StencilOptions.hpp"
+
+#include "gl/OpenGLPointers.hpp"
 
 namespace balls {
 
-struct OpenGLPointers;
+class BlendState;
+class ClipOptions;
+class ColorOptions;
+class DepthOptions;
+class GeometryOptions;
+class Hints;
+class ImplementationInfo;
+class SampleOptions;
+class StencilOptions;
 
 class OpenGLStateWidget : public QWidget {
   Q_OBJECT
 
 public:
-  explicit OpenGLStateWidget(OpenGLPointers&, QWidget* = nullptr);
+  explicit OpenGLStateWidget(QWidget* = nullptr);
+
+  void setOpenGLPointers(const OpenGLPointers&) noexcept;
 
 private /* fields */:
   Ui::OpenGLStateWidget ui;
 
-  BlendState m_blendState;
-  ClipOptions m_clipOptions;
-  ColorOptions m_colorOptions;
-  DepthOptions m_depthOptions;
-  GeometryOptions m_geometryOptions;
-  Hints m_hints;
-  ImplementationInfo m_implementationInfo;
-  SampleOptions m_sampleOptions;
-  StencilOptions m_stencilOptions;
+  OpenGLPointers m_gl;
+
+  BlendState* m_blendState;
+  ClipOptions* m_clipOptions;
+  ColorOptions* m_colorOptions;
+  DepthOptions* m_depthOptions;
+  GeometryOptions* m_geometryOptions;
+  Hints* m_hints;
+  ImplementationInfo* m_implementationInfo;
+  SampleOptions* m_sampleOptions;
+  StencilOptions* m_stencilOptions;
 };
 }
 
