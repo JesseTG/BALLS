04e31b9b7e8421cd348b9185e4d4e4f3d7ffa170
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2016-08-19 01:05:42 -0400

    Be more choosy about Qt installer components

--- qt-installer-console.js
+++ qt-installer-console.js
@@ -28,8 +28,31 @@ Controller.prototype.TargetDirectoryPageCallback = function()
 
 Controller.prototype.ComponentSelectionPageCallback = function() {
     var widget = gui.currentPageWidget();
-    widget.selectAll();
+    widget.deselectAll();
+    widget.selectComponent("qt.56.win64_msvc2015_64.qtbase");
+    widget.selectComponent("qt.56.win64_msvc2015_64.qtwinextras");
+    widget.selectComponent("qt.56.win64_msvc2015_64.qtimageformats");
+    widget.selectComponent("qt.56.win64_msvc2015_64.qttools");
+    widget.selectComponent("qt.56.win64_msvc2015_64.icu_541_release.win_64");
+    widget.selectComponent("qt.56.win64_msvc2015_64.d3d_compiler_47.x64");
+    widget.selectComponent("qt.56.win64_msvc2015.opengl32sw.x64");
+    widget.selectComponent("qt.56.win64_msvc2015_64.qttranslations");
+    widget.selectComponent("qt.56.qtquickcontrols.win64_msvc2015_64");
+    widget.selectComponent("qt.56.qtquickcontrols2.win64_msvc2015_64");
+    widget.selectComponent("qt.56.win64_mingw492.essentials");
+    widget.selectComponent("qt.56.win64_mingw492.addons");
+    widget.selectComponent("qt.56.win64_mingw492.icu_541.mingw_64");
+    widget.selectComponent("qt.56.win64_mingw492.runtime");
 
+    widget.selectComponent("qt.57.win64_msvc2015_64.qtbase");
+    widget.selectComponent("qt.57.win64_msvc2015_64.qtwinextras");
+    widget.selectComponent("qt.57.win64_msvc2015_64.qtimageformats");
+    widget.selectComponent("qt.57.win64_msvc2015_64.qttools");
+    widget.selectComponent("qt.57.win64_msvc2015_64.qtquickcontrols");
+    widget.selectComponent("qt.57.win64_msvc2015_64.qtquickcontrols2");
+    widget.selectComponent("qt.57.win64_msvc2015_64.d3d_compiler_47.x64");
+    widget.selectComponent("qt.57.win64_msvc2015.opengl32sw.x64");
+    
     gui.clickButton(buttons.NextButton);
 }
 
